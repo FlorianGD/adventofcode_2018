aedeb6c7688d8df418fd3e398acaa199778c4460
Florian Gaudin-Delrieu <florian.gaudindelrieu@gmail.com>
2018-12-03 13:39:49 +0100

    day02 part 2

--- day03.py
+++ day03.py
@@ -43,3 +43,34 @@ assert compute_overlap(updated_test_grid) == 4
 
 part_one_solution = compute_overlap(update_grid())
 print(f'Solution for part 1: {part_one_solution}')
+
+# Part 2
+# Now we need to find the only id with no overlap.
+# So, I need to change the regex to get the id also, and to change the update
+# process as well.
+
+PATTERN = r'(?P<id>\d+) @ (?P<l>\d+),(?P<t>\d+): (?P<w>\d+)x(?P<h>\d+)'
+COORDS_ID = re.compile(PATTERN)
+
+
+def parse_instructions_id(line, regex=COORDS_ID):
+    '''Parse a line of instructions'''
+    m = regex.search(line)
+    return int(m['id']), int(m['l']), int(m['t']), int(m['w']), int(m['h'])
+
+
+def find_non_overlap(grid=GRID, lines=read_input()):
+    """
+    The grid is already updated (it was updated in place with the first part)
+    I will scan all the lines again and check if the subgrid has only ones.
+    If so I will return the id.
+    """
+    for line in lines:
+        id, left, top, width, height = parse_instructions_id(line)
+        if np.all(grid[left:left+width, top:top+height] == 1):
+            return id
+
+
+assert find_non_overlap(grid=test_grid, lines=test_input) == 3
+
+print(f'Solution for part 2: {find_non_overlap(grid=GRID)}')
